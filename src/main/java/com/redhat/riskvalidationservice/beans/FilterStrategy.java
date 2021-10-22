93e2ec5c12da901a962dcc690c822900faa07190
snandakumar87 <snandaku@redhat.com>
2021-10-22 00:26:50 -0400

    commit

--- src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
+++ src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
@@ -75,7 +75,7 @@ public class FilterStrategy implements AggregationStrategy {
                         dmnContext.get("Interval"));
 
                 ServiceResponse<DMNResult> serverResp =
-                        dmnClient.evaluateAll("DMNListExample",
+                        dmnClient.evaluateAll("EventAutomationDecision_1.0.0",
                                 namespace,
                                 modelName,
                                 dmnContext);
