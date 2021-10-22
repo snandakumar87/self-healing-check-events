c731ace240fc7e8aed65c3fcf18c469295085888
snandakumar87 <snandaku@redhat.com>
2021-10-21 23:14:17 -0400

    commit

--- src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
+++ src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
@@ -60,7 +60,7 @@ public class FilterStrategy implements AggregationStrategy {
 
                 DMNContext dmnContext = dmnClient.newContext();
                 dmnContext.set("SensuEvents", sensuEvents);
-                if (null != runs) {
+                if (null != runs && runs.get(example.getEntity().getSystem().getHostname()) != null) {
                     dmnContext.set("ApbRuns", runs.get(example.getEntity().getSystem().getHostname()));
                     System.out.println(runs.get(example.getEntity().getSystem().getHostname()));
                 } else {
