67131c2e248a855fd422457cc7850a1b64558006
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2021-10-28 15:12:45 -0400

    Update FilterStrategy.java

--- src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
+++ src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
@@ -32,13 +32,13 @@ public class FilterStrategy implements AggregationStrategy {
             org.springframework.http.HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.set("accept","application/json");
             httpHeaders.set("content-type","application/json");
-           // httpHeaders.set("Authorization","Basic YWRtaW5Vc2VyOmM3bHB4WExh");
-            httpHeaders.set("Authorization","Basic YWRtaW5Vc2VyOlJlZEhhdA==");
+            httpHeaders.set("Authorization","Basic YWRtaW5Vc2VyOmM3bHB4WExh");
+           // httpHeaders.set("Authorization","Basic YWRtaW5Vc2VyOlJlZEhhdA==");
 
 
 
-         //   CredentialsProvider credentialsProvider = new EnteredCredentialsProvider("adminUser", "c7lpxXLa");
-            CredentialsProvider credentialsProvider = new EnteredCredentialsProvider("adminUser", "RedHat");
+         CredentialsProvider credentialsProvider = new EnteredCredentialsProvider("adminUser", "c7lpxXLa");
+            //CredentialsProvider credentialsProvider = new EnteredCredentialsProvider("adminUser", "RedHat");
             
             
 
