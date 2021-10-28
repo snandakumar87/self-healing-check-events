4120f6da1220f8087a51060f29d39af9d6660043
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2021-10-28 14:13:16 -0400

    Update FilterStrategy.java

--- src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
+++ src/main/java/com/redhat/riskvalidationservice/beans/FilterStrategy.java
@@ -124,7 +124,7 @@ public class FilterStrategy implements AggregationStrategy {
                     apb.setCheckName(sensuEvents.getCheckType());
                     apb.setHostName(sensuEvents.getHostName());
                     apb.setRunDate(new Date().getTime());
-                    System.out.printn("apb"+new Gson().toJson(apb));
+                    System.out.println("apb"+new Gson().toJson(apb));
 
 
                     newExchange.getIn().setBody(new Gson().toJson(apb));
